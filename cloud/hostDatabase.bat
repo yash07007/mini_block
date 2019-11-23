073d68b4f5d975c0870adf9aa444d132d9c18427
Yash Solanki <yashsolanki777@gmail.com>
2019-11-23 16:06:49 +0530

    renaming cloud

---
+++ cloud/hostDatabase.bat
@@ -0,0 +1,2 @@
+cd "C:\Program Files\MongoDB\Server\4.0\bin"
+mongod.exe --dbpath "C:\Users\DELL\Desktop\Mini Block\cloud\cloud-data"
--- democloud/hostDatabase.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\Program Files\MongoDB\Server\4.0\bin"
-mongod.exe --dbpath "C:\Users\DELL\Desktop\Mini Block\democloud\cloud-data"
